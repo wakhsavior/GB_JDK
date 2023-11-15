10855c873a5ecf89726844ac2edb6b09544506f0
Saveliev Denis <wakhsavior@gmail.com>
2023-11-15 17:13:19 +0300

    Add Game TicTacToe Lection 01

---
+++ src/main/java/net/ddns/wakhsavior/GameWindow.java
@@ -0,0 +1,51 @@
+package net.ddns.wakhsavior;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GameWindow extends JFrame {
+    private static final int WINDOW_HEIGHT = 555;
+    private static final int WINDOW_WIDTH = 507;
+    private static final int WINDOW_POSX = 800;
+    private static final int WINDOW_POSY = 300;
+    JButton btnStart = new JButton("New Game");
+    JButton btnExit = new JButton("Exit");
+    Map map;
+    SettingsWindow settings;
+
+    GameWindow() {
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLocation(WINDOW_POSX, WINDOW_POSY);
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setTitle("TicTacToe");
+        setResizable(false);
+
+
+        map = new Map();
+        settings = new SettingsWindow(this); //Объек в качестве ссылки передает себя
+        btnExit.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+        btnStart.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                settings.setVisible(true);
+            }
+        });
+        JPanel panBottom = new JPanel(new GridLayout(1, 2));
+        panBottom.add(btnStart);
+        panBottom.add(btnExit);
+        add(panBottom, BorderLayout.SOUTH);
+        add(map);
+        setVisible(true);
+
+    }
+    void startNewGame(int mode, int fSzX, int fSzY, int wLen){
+        map.startNewGame(mode, fSzX, fSzY, wLen);
+    }
+}
